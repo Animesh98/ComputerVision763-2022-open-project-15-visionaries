cdf00a623461bc2dc4e276f876c067482923b7d4
sancharp <31501422+sancharpalit@users.noreply.github.com>
2022-04-07 12:59:32 +0530

    Update model_train.py

--- code/model_train.py
+++ code/model_train.py
@@ -2,7 +2,6 @@ import tensorflow as tf
 import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
-from tensorflow.keras.callbacks import TensorBoard
 from train import preprocessData, loadData
 
 
