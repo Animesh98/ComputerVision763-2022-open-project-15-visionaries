26081f647b12d6555f8896987e79d2e7f1c4d547
sancharp <31501422+sancharpalit@users.noreply.github.com>
2022-04-07 12:58:05 +0530

    Update test.py

--- code/test.py
+++ code/test.py
@@ -10,7 +10,7 @@ from sklearn.metrics import multilabel_confusion_matrix, accuracy_score
 actions = np.array(['up', 'down', 'left', 'right', 'jump'])
 
 
-###LSTM model#######
+### Load weights #######
 model = Sequential()
 model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(20,63)))
 model.add(LSTM(128, return_sequences=True, activation='relu'))
@@ -21,12 +21,11 @@ model.add(Dense(actions.shape[0], activation='softmax'))
 
 model.load_weights('../weights/action.h5')
 
+### Load data #######
 data, labels = loadData('dataPoints', 'labels')
 x_train, x_test, y_train, y_test = preprocessData(data, labels)
 
-#actions[np.argmax(res[4])]
-#actions[np.argmax(y_test[4])]
-
+#### Test ########
 yhat = model.predict(x_test)
 
 ytrue = np.argmax(y_test, axis=1).tolist()
