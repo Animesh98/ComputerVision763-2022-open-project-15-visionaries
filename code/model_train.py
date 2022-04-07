e16891418cfb3c5a18b23dc12604b5fc797ded89
sancharp <31501422+sancharpalit@users.noreply.github.com>
2022-04-07 12:54:47 +0530

    Update and rename model.py to model_train.py

--- code/model_train.py
+++ code/model_train.py
@@ -5,14 +5,11 @@ from tensorflow.keras.layers import LSTM, Dense
 from tensorflow.keras.callbacks import TensorBoard
 from train import preprocessData, loadData
 
-#log_dir = os.path.join('Logs')
-#tb_callback = TensorBoard(log_dir=log_dir)
 
 actions = np.array(['up', 'down', 'left', 'right', 'jump'])
-#tf.debugging.set_log_device_placement(True)
 
 
-###Build and train LSTM#######
+###Build Model#######
 model = Sequential()
 model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(20,63)))
 model.add(LSTM(128, return_sequences=True, activation='relu'))
@@ -21,10 +18,11 @@ model.add(Dense(64, activation='relu'))
 model.add(Dense(32, activation='relu'))
 model.add(Dense(actions.shape[0], activation='softmax'))
 
-
+###Load data#####
 data, labels = loadData('dataPoints', 'labels')
 x_train, x_test, y_train, y_test = preprocessData(data, labels)
 
+##### Train and save weights ########
 model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
 model.fit(x_train, y_train, epochs=800)
 model.summary()
