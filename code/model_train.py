2f8e2127ec32e942ce89d00280a139d119ad8e66
sanchar <31501422+sancharpalit@users.noreply.github.com>
2022-04-09 19:08:06 +0530

    Add files via upload

--- code/model_train.py
+++ code/model_train.py
@@ -1,28 +1,33 @@
 import tensorflow as tf
 import numpy as np
+from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.callbacks import TensorBoard
 from train import preprocessData, loadData
+from mymodel import *
 
 
-actions = np.array(['up', 'down', 'left', 'right', 'jump'])
+#actions = np.array(['up', 'down', 'left', 'right', 'jump', 'stop'])
 
 
-###Build Model#######
-model = Sequential()
-model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(20,63)))
-model.add(LSTM(128, return_sequences=True, activation='relu'))
-model.add(LSTM(64, return_sequences=False, activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(32, activation='relu'))
-model.add(Dense(actions.shape[0], activation='softmax'))
+###Build and train model#######
+#model = Sequential()
+#model.add(layers.Bidirectional(layers.LSTM(64, return_sequences=True), input_shape=(20, 105)))
+#model.add(layers.Bidirectional(layers.LSTM(128, return_sequences=True, activation='relu')))
+#model.add(layers.Bidirectional(layers.LSTM(64, return_sequences=False, activation='relu')))
+#model.add(layers.Dense(64, activation = 'relu'))
+#model.add(layers.Dense(32, activation = 'relu'))
+#model.add(layers.Dense(32, activation = 'relu'))
+#model.add(layers.Dense(actions.shape[0], activation='softmax'))
 
-###Load data#####
-data, labels = loadData('dataPoints', 'labels')
+model = MyModel()
+
+data, labels = loadData('dataPointsMotion_1', 'labelsMotion_1')
 x_train, x_test, y_train, y_test = preprocessData(data, labels)
 
-##### Train and save weights ########
+
 model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
-model.fit(x_train, y_train, epochs=800)
+model.fit(x_train, y_train, epochs=400)
 model.summary()
-model.save('../weights/action.h5')
+model.save('../weights/actionMotion_1')
